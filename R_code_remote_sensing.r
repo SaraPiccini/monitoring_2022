2767fd43ee609a516895508b551d4f5736e54c4e
Sara Piccini <92572907+SaraPiccini@users.noreply.github.com>
2021-10-25 11:03:03 +0200

    Create R_code_remote_sensing.r

---
+++ R_code_remote_sensing.r
@@ -0,0 +1,9 @@
+#R code for ecocystem monitoring by remote sensing 
+#First of all, we need to install additional packages: raster
+#raster package to manage image data
+# https://cran.r-project.org/web/packages/raster/index.html
+#quad when the package is outside R (install raster), no quads when it's already inside R (library)
+
+install.packages("raster")
+
+library(raster)
