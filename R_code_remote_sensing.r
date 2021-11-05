67836e59f9abbb327e6b92cb55e9f2f1b1e48194
Sara Piccini <92572907+SaraPiccini@users.noreply.github.com>
2021-11-05 11:05:00 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -24,7 +24,7 @@ setwd("/Users/sarapiccini/Desktop/lab")
 #SDX
 #we are going to import satallite data, PUTTING QUADS BECAUSE WE ARE GOING TO CATCH DATA OUTSIDE R
 
-l2011 <- brick("p224r63_2011_masked.grd")
+l2011 <- brick("p224r63_2011.grd")
 
 #Since we want to use this data in the future we put a name in front of the function
 #Landsat is the satellite from 1972 cover the world every 14 days
@@ -44,7 +44,8 @@ plot(l2011)
 #plot the data in order to see landscape (amazon forest) composed by different bands. Reflectances of bands  
 #B1 is the amount of reflectance in the blue band
 #B2 the amount of refelctance in the green band
-#B3 red band
+#B3 is the reflectans in the red band
+#B4 is the reflectans in the NIR band
 #B1 B2 B3 are the reflectance is the visible bands
 #color palette can change in this way: 
 #change the colors that we want to use to show the differend band
@@ -72,3 +73,62 @@ plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
 #natural color image: how human see landscape since we are using oly green blue and red bands.
 
 #associated
+
+library(raster)
+#set the folder in which data are: called working directory - set the working directory:
+#function for working directory:
+setwd("/Users/sarapiccini/Desktop/lab")
+l2011 <- brick("p224r63_2011.grd") 
+#grd:greed (set of pixel) #brick function creates a raster file: RasterBrick
+
+#B1 is the amount of reflectance in the blue band called (B1_sre)
+#B2 is the amount of refelctance in the green band called (B2_sre)
+#B3 is the reflectans in the red band called (B3_sre)
+#B4 is the reflectans in the NIR (near infra-red) band called (B4_sre)
+#each pixel has the reflectans in a certain band 
+#pixel resolution is 30 m
+
+#plot the green band(B2)
+#see the name of this band in our file: B2_sre(spectral reflectance)
+#object:l2011
+#how to link the object and the name of the file of the green band in R: use the symbol $
+plot(l2011$B2_sre) 
+#plot is the function, the argument is the thing inside the parenthesis
+#to change it change the skin of the colors with this function:
+#the colorRampPalette function is estending the color palette to another color (we can choose the color we want)
+cl <- colorRampPalette (c("black", "grey", "light grey"))(100)
+#everything that absorb the green light (no reflectans=0) will become black, light grey means a lor of reflectans of the green band
+#grey for medium reflectans of green light
+#100 = numbers od different color we use from black to light grey
+#narrow to associate the object (colorRampPalette) to a name (cl)
+
+plot(l2011$B2_sre, col=cl)
+
+#change the colorRampPalette from dark green to light green:
+clg <- colorRampPalette (c("dark green", "green", "light green"))(100)
+plot(l2011$B2_sre, col=clg)
+
+#do the same for the blue band using dark blue , blue, and light blue
+#name of the blue band (we can see this on r): B1_sre
+clb <- colorRampPalette (c("dark blue", "blue", "light blue")) (100)
+plot(l2011$B1_sre, col=clb)
+
+#function "par": set or query graphical parametres: can change 
+#multiframe: put first and second plot in one frame: we put one row and two colomns 
+#oper a new window called multiframe and we put the green and blue plot in one row and two colomns
+#plot both images in just one multiframe graph:
+par(mfrow=c(1,2)) #1 row and 2 colomns (the first number is the number of rows in the multiframe 
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+
+par(mfrow=c(2,1)) #2 rows and 1 colomn 
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+
+#colors do not exist, they're just a convention, they're just in out brain
+
+#to reverse the pattern and have the green before the blue, since it's a sequence of function:
+par(mfrow=c(2,1))
+plot(l2011$B2_sre, col=clg)
+plot(l2011$B1_sre, col=clb)
+
