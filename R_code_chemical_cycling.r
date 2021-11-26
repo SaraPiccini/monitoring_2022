eddf648b51fd8d30b13ec54d7f4561c6ac36030e
Sara Piccini <92572907+SaraPiccini@users.noreply.github.com>
2021-11-26 09:44:30 +0100

    Create R_code_chemical_cycling.r

---
+++ R_code_chemical_cycling.r
@@ -0,0 +1,8 @@
+# R code for chemical cycling study
+# time serie of NO2 change in EU during lockdown
+# functions useful to import data: 
+# possible range 
+# use brick functions to take data from folders outside R, but since we have single data (single layers we use raster function)
+# every single image has one layer
+# use raster function: create a laster layer
+
