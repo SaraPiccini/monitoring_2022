1d5a811a3f0f9ebc7f3d66a7aa5c4d133e29c598
Sara Piccini <92572907+SaraPiccini@users.noreply.github.com>
2021-12-06 09:08:54 +0100

    Update R_code_quantitative_estimates_land_coverr.r

--- R_code_quantitative_estimates_land_coverr.r
+++ R_code_quantitative_estimates_land_coverr.r
@@ -1,4 +1,7 @@
 R_code_quantitative_estimates_land_cover
+# R_code_quantitative_estimates_land_cover
+# forest: amount of cluster of trees, 3D structure forest
+# package use
 library(raster)
 library(RStoolbox)
 library(ggplot2)
