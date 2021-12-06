e85f2b7b6b63147957974842013258a5f8632dc9
Sara Piccini <92572907+SaraPiccini@users.noreply.github.com>
2021-12-06 10:57:05 +0100

    Update R_code_quantitative_estimates_land_cover.r

--- R_code_quantitative_estimates_land_cover.r
+++ R_code_quantitative_estimates_land_cover.r
@@ -82,3 +82,98 @@ ggplot(proportion1992, aes(x=cover, y=prop1992, color=cover)) + geom_bar(stat="i
 #aesthetics: part which are coloring the graph
 #create an histogram for example
 #geom_bar function to make bars and so bar charts (type of the graph)
+
+#day 2
+library(raster)
+> library(RStoolbox)
+> library(ggplot2)
+> setwd("/Users/sarapiccini/Desktop/lab")
+rlist <- list.files(pattern="defor")
+rlist
+#[1] "defor1_.jpg" "defor2_.jpg"
+#list_rast <- lapply(rlist, brick) #lapply(x, FUN)
+#list_rast
+l1992 <- list_rast[[1]]
+l1992
+l2006 <- list_rast[[2]]
+l2006
+plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
+l1992c <- unsuperClass(l1992, nClasses=2) #unsuperClass(x, nClasses) function + (argument: name of file and number of classes)
+l1992c
+plot(l1992c$map)
+freq(l1992c$map)
+#     value  count
+#[1,]     1  35300
+#[2,]     2 305992
+> total <- 341292
+
+propagri <- 35300 / 341292
+propforest <- 305992 / total
+propagri = 0.1034305 #~ 0.10
+propforest = 0.8965695 #~ 0.90
+#build a datafram 
+cover <- c("Forest", "Agriculture")
+prop1992 <- c(propforest, propagri) #istead of putting the number, we put objects
+> proportion1992 <- data.frame(cover, prop1992) #propootions of pixels in 1992
+> proportion1992
+        cover  prop1992
+1      Forest 0.8965695
+2 Agriculture 0.1034305
+> ggplot(proportion1992, aes(x=cover, y=prop1992, color=cover)) + geom_bar(stat="identity", fill="white")
+> l2006c <- unsuperClass(l2006, nClasses=2) 
+> l2006c
+
+plot(l2006c$map)
+freq(l2006c$map)
+#     value  count
+#[1,]     1 164230
+#[2,]     2 178496
+total <- 342726 
+propagri <- 164230 / 342726
+propforest <- 178496 / total
+propagri
+#[1] 0.4791875
+propforest
+#[1] 0.5208125
+propagri = 0.4791875
+propforest = 0.5208125
+prop2006 <- c(propforest, propagri)
+prop2006
+#[1] 0.5208125 0.4791875
+propagri2006 = 0.4791875
+propforest2006 = 0.5208125
+prop2006 <- c(propforest2006, propagri2006) #instead of putting the number, we put objects (always better to use it instead of numbers)
+proportion2006 <- data.frame(cover, prop2006)
+proportion2006
+#        cover  prop2006
+#1      Forest 0.5208125
+#2 Agriculture 0.4791875
+total2006 <- 342726 
+# proportions:
+propagri2006 <- 164230 / 342726
+propforest2006 <- 178496 / total
+propagri2006 = 0.4791875
+propforest2006 = 0.5208125
+#dataframe:
+cover <- c("Forest", "Agriculture")
+prop2006 <- c(propforest2006, propagri2006) #instead of putting the number, we put objects (always better to use it instead of numbers)
+proportion2006 <- data.frame(cover, prop2006) #propootions of pixels in 1992
+proportion2006 
+        cover  prop2006
+#1      Forest 0.5208125
+#2 Agriculture 0.4791875
+proportion <- data.frame(cover, prop1992, prop2006)
+proportion
+ #       cover  prop1992  prop2006
+#1      Forest 0.8965695 0.5208125
+#2 Agriculture 0.1034305 0.4791875
+
+
+ggplot(proportion2006, aes(x=cover, y=prop2006, color=cover)) + geom_bar(stat="identity", fill="white")
+ggplot(proportion, aes(x=cover, y=prop2006, color=cover)) + geom_bar(stat="identity", fill="white")
+
+library(gridExtra)
+
+p1 <- ggplot(proportion1992, aes(x=cover, y=prop1992, color=cover)) + geom_bar(stat="identity", fill="white")
+p2 <- ggplot(proportion2006, aes(x=cover, y=prop2006, color=cover)) + geom_bar(stat="identity", fill="white")
+grid.arrange(p1, p2, nrow=1)
