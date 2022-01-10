a49ba384f2010e66cf6efe10315995638a39f91f
Sara Piccini <92572907+SaraPiccini@users.noreply.github.com>
2022-01-10 10:46:38 +0100

    Update R_code_sdm.r

--- R_code_sdm.r
+++ R_code_sdm.r
@@ -1,20 +1,69 @@
 # R code for species distribution modelling mainly distribution on individuals of a population in space
-install.packages("sdm") #species distribution models 
-library(sdm)
+install.packages("sdm") #species distribution modelling package
+library(sdm) #spatial package
 # we going to use this for populations' studies: check for single species: individuals from same species
 # we use data inside tha package, we do not need to set the working directory since data are already in
-library(raster) #predictors
-library(rgdal) #geospatial data abstraction library - species
+library(raster) #predictors: environmental variable - help in predicting species distributions
+library(rgdal) #geospatial data abstraction library - species - coordinate data with value (vector data: array of coordinates)
 
 #use of "system.file" function that shows all the files in a certain package called sdm
-file <- system.file("external/species.shp", package="sdm")
+file <- system.file("external/species.shp", package="sdm") #species data (path, package used and name it) 
 #shp extention: shape file-point related to the species(presence-absence)
+#prj = info about coordinate system of out data UTM-zones
+#dbf = 
+#shx = links every data to a line
 #explain the name of the file we are taking the files
 #path in which we have the data:"/Library/Frameworks/R.framework/Versions/4.1/Resources/library/sdm/external/species.shp"
 #if we take this path and we put it in our file systems we will find our species data
+file #we can see the path -> inside the external folder there are the data
+#recreating a shape file inside r with the shapefile function, we are importing this data
+species <- shapefile(file) #exactly as the raster function for raster files (correspondence function of raster). These are points(vector of coordinates)
+species #datarame, each row of the table are point: spatial point dataframe 
+#200 points inside , 1 variable (called occurence: 0 r 1 if you find or not the species)
+species$Occurrence #200 occurences with 1 or 0 to each species
+#language used for subsetting data
+#how many occurrences are there? use [] to make a subset - if you want to subset the occurerences == 1 or 0 (for negative query(!=)
+species[species$Occurrence == 1,] #control symbol: used to explain that the query has finished : the comma (,)
+#94 are the occurrences = 1
+species[species$Occurrence == 0,] #200-94=106
+#we can built a new dataframe with only presences assigning it to a new object: -  manner to subset DataFrame
+presences <- species[species$Occurrence == 1,]
+absences <- species[species$Occurrence == 0,]
+#plot
+plot(species, pch=19) #points scattered in space - distribution of species in an area
+#plotting only presences of the species
+plot(presences, pch=19, col="blue") #lower amount of points since we only have presences
+#add to the presences plot also the absences: add additional points to the previous plot - plot together presences and absences
+points(absences, pch=19, col="red") #plot in-situ data
+#presences in blue and absences in red
+#Probability to find species or not - species distribution model based on raster dataset (T+elevation+water availability) - use of predictors
+#look at the predictors
+#raster data: path in which they are located: only external not specifying single files because we want all
+path <- system.file("external", package="sdm")
+path #folder in which rasterdata - all predictors have same extention (ASC): ASCII useful extention (sort of txt) - code for info interchange
+#predictors are contained inside same folderrs of species but with extention asc  
+#list files function: list files inside a certain folder (path in our case-external folder) - common pattern for files: asc
+lst <- list.files(path, pattern="asc", full.names=TRUE) #list of the files -> vegetation T elevation precipitation
+#you can use ethe lapply function with the raster function but in this case it is not needed since data are inside the package and they have an asc extention
+#stack of the list 
+preds <- stack(lst)
+preds
+#plot predictors: change colorRampPalette - to high light max values put  YELLOW color
+cl <- colorRampPalette(c('blue','orange','red','yellow')) (100)
+plot(preds, col=cl) 
 
-#recreating a ship file inside r with the shapefile function, we are importing this data
-species <- shapefile(file) #exactly as the raster function for raster files. These are points(vector of coordinates)
-plot(species, pch=19, col="red") #points scattered in space 
+plot(preds$elevation, col=cl)
+points(presences, pch=19) #most of the points are in the low elevation parts and not in the peaks
+
+plot(preds$temperature, col=cl)
+points(presences, pch=19) #it will be found at higher T - not at very low T
+
+plot(preds$vegetation, col=cl)
+points(presences, pch=19) #where there is vegetation cover and the species avoid parts where there is no vegetation
+
+plot(preds$precipitation, col=cl)
+points(presences, pch=19) #distribution of the species: medium intermediate precipitation level
+
+#model: 
 
 
