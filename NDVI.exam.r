029b6731b629939af899669b6970d4f3072254c3
Sara Piccini <92572907+SaraPiccini@users.noreply.github.com>
2022-02-04 19:30:54 +0100

    Update NDVI.exam.r

--- NDVI.exam.r
+++ NDVI.exam.r
@@ -1,10 +1,11 @@
 # R code for my Monitoring Ecosystem Changes and Functioning exam in 2022
-# multi-temporal analysis in vegetation state in Central Africa
+# Multi-temporal analysis on vegetation in Indonesia and Papua New Guinea with a focus on deforstation 
+# Download data from Copernicus Global Land Service on NDVI and FCOVER 
 
 # Upload all the libraries needed at the beginning - for uploading and visualizing copernicus data in R
 library(ncdf4) # for formatting our files - to manage spatial data, read and manipulate them (in nc)
 library(raster) # work with raster file (single layer data)
-library(RStoolbox) # tools for remote sensing data processing; builds on raster and make classification
+#library(RStoolbox) # tools for remote sensing data processing; builds on raster and make classification
 library(ggplot2) # for plots - to ggplot raster layers - create graphics
 library(viridis) # palette -  color scales
 library(patchwork) # for comparing separate ggplots - compose multiple ggplots
@@ -18,7 +19,7 @@ NDVI1999 <- raster("c_gls_NDVI_199906010000_GLOBE_VGT_V2.2.1.nc")
 NDVI1999
 
 # we can also import multiple files at once that have the same pattern in the name (much faster when we have many files to import)
-rlist <- list.files(pattern="NDVI") # listing all the files with the pattern present in the directory
+rlist <- list.files(pattern="gls_NDVI") # listing all the files with the pattern present in the directory
 rlist
 list_rast <- lapply(rlist, raster) # to make the list a brick list - apply brick function to all the files (multi-layers)
 list_rast
@@ -26,54 +27,78 @@ NDVIstack <- stack(list_rast) # creating a stack
 NDVIstack
 plot(NDVIstack)
 
-ext <- c(-27, 47, -20, 16)
+ext <- c(94.5, 150, -11.5, 0)
 NDVIcrop <- crop(NDVIstack, ext)
 plot(NDVIcrop)
 NDVIcrop
 
 # Let's assign them to an object
-NDVI1999 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1KM.1
-NDVI2004 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1KM.2
-NDVI2009 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1KM.3
-NDVI2014 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1KM.4
-NDVI2019 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1KM.5
+NDVI1999 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1km.1
+NDVI2004 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1km.2
+NDVI2009 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1km.3
+NDVI2014 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1km.4
+NDVI2019 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1km.5
 
-g1 <- ggplot() + geom_raster(data= NDVI1999, mapping = aes(x=x, y=y, fill= Normalized.Difference.Vegetation.Index.1KM.1)) + scale_fill_viridis(option = "magma") + ggtitle("NDVI in 1999")
-g2 <- ggplot() + geom_raster(data= NDVI2004, mapping = aes(x=x, y=y, fill= Normalized.Difference.Vegetation.Index.1KM.2)) + scale_fill_viridis(option = "magma") + ggtitle("NDVI in 2004")
-g3 <- ggplot() + geom_raster(data= NDVI2009, mapping = aes(x=x, y=y, fill= Normalized.Difference.Vegetation.Index.1KM.3)) + scale_fill_viridis(option = "magma") + ggtitle("NDVI in 2009")
-g4 <- ggplot() + geom_raster(data= NDVI2014, mapping = aes(x=x, y=y, fill= Normalized.Difference.Vegetation.Index.1KM.4)) + scale_fill_viridis(option = "magma") + ggtitle("NDVI in 2014")
-g5 <- ggplot() + geom_raster(data= NDVI2019, mapping = aes(x=x, y=y, fill= Normalized.Difference.Vegetation.Index.1KM.5)) + scale_fill_viridis(option = "magma") + ggtitle("NDVI in 2019")
+# Plot them with ggplot 
+g1 <- ggplot() + geom_raster(data = NDVI1999, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km.1)) + scale_fill_viridis(option = "inferno") + ggtitle("NDVI in 1999") + labs(fill = "NDVI")
+g2 <- ggplot() + geom_raster(data = NDVI2004, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km.2)) + scale_fill_viridis(option = "inferno") + ggtitle("NDVI in 2004") + labs(fill = "NDVI")
+g3 <- ggplot() + geom_raster(data = NDVI2009, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km.3)) + scale_fill_viridis(option = "inferno") + ggtitle("NDVI in 2009") + labs(fill = "NDVI")
+g4 <- ggplot() + geom_raster(data = NDVI2014, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km.4)) + scale_fill_viridis(option = "inferno") + ggtitle("NDVI in 2014") + labs(fill = "NDVI")
+g5 <- ggplot() + geom_raster(data = NDVI2019, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km.5)) + scale_fill_viridis(option = "inferno") + ggtitle("NDVI in 2019") + labs(fill = "NDVI")
 
-#
+# I have also tried this one: scale_fill_viridis(direction = -1, option = "magma") with the order of colors reversed but 
+# Plot them together (multiframe ggplot) with grid.arrange function (from gridExtra package)
+grid.arrange(g1, g2, g3, g4, g5, nrow=3)
+#or with patchwork package 
+g2 + g1 + g3 / g4 + g5
 
+# Export file
+
+pdf("NDVIggplot.pdf", width=30, height=10)
+grid.arrange(g1, g2, g3, g4, g5, nrow=3)
+dev.off()
+
+# I have created a color ramp palette to better differences
+clNDVI <- colorRampPalette(colors = c('#6e462c', '#9c8448', 'white', '#9cab68', '#306466'))(255)
+
+par(mfrow=c(2,3))
+plot(NDVI1999, col = clNDVI)
+plot(NDVI2004, col = clNDVI)
+plot(NDVI2009, col = clNDVI)
+plot(NDVI2014, col = clNDVI)
+plot(NDVI2019, col = clNDVI)
+
+#ndvi difference between years
+NDVIdif <- NDVI1999 - NDVI2019
+#creation of a new color palette to emphasize differences with COLORBREWER 2.0:
+col <- colorRampPalette(c('#ef3b2c','#f7f7f7','#008837'))(255)
+plot(NDVIdif, col=col, main="Difference in NDVI between 1999 and 2019")
+
+
+############### class
 #list_rast2 <- lapply(rlist, brick) # to make the list a brick list - apply brick function to all the files (multi-layers)
 #list_rast2
-
-
 list_rast <- lapply(rlist, raster) # to make the list a brick list - apply brick function to all the files (multi-layers)
 list_rast
 NDVIstack <- stack(list_rast) # creating a stack
 NDVIstack
-ext <- c(-27, 47, -20, 16)
+plot(NDVIstack)
+ext <- c(-5, 25, 0, 10)
 NDVIcrop <- crop(NDVIstack, ext)
 plot(NDVIcrop)
 NDVIcrop
-
 NDVI1999 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1KM.1
 NDVI2004 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1KM.2
 NDVI2009 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1KM.3
 NDVI2014 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1KM.4
 NDVI2019 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1KM.5
-
 NDVI1999 <- raster("c_gls_NDVI_199906010000_GLOBE_VGT_V2.2.1.nc")
 plotRGB(NDVI1999, r=1, g=2, b=3, stretch="lin")
 ggRGB(NDVI1999, r=1, g=2, b=3, stretch="lin")
 ext <- c(380, 450, 140, 200)
 LC2001crop <- crop(LC2001, ext)
 ggRGB(LC2001crop, r=1, g=2, b=3, stretch="lin")
-
-
-NDVI1999c <- unsuperClass(NDVI1999, nClasses=7)
+NDVI1999c <- unsuperClass(NDVI1999, nClasses=5, clusterMap=TRUE, nStarts = 1)
 NDVI1999c
 plot(NDVI1999c$map)
 NDVI2004c <- unsuperClass(NDVI2004, nClasses=4)
@@ -85,29 +110,20 @@ plot(NDVI2014c$map)
 NDVI2014c <- unsuperClass(NDVI2014, nClasses=4)
 NDVI2014c
 plot(NDVI2014c$map)
-NDVI2019c <- unsuperClass(NDVI2019, nClasses=7)
+NDVI2019c <- unsuperClass(NDVI2019, nClasses=5, clusterMap=TRUE, nStarts = 1)
 NDVI2019c
 plot(NDVI2019c$map)
-
 par(mfrow=c(1,2))
 plot(NDVI1999c$map)
 plot(NDVI2019c$map)
-
-
-
 grid.arrange(g1, g2, g3, g4, g5, nrow=2)
 
-
-
-
-
-
 ### FCOVER ###
-
+# Same passages of before
 rfcover <- list.files(pattern="FCOVER")
 rfcover
 
-rfcover_rast <- lapply(rfcover, raster) # same as doing fcover_import <- lapply(fcover_list, brick, varname="FCOVER") because the first layer is FCOVER
+rfcover_rast <- lapply(rfcover, raster)
 rfcover_rast 
 
 # Creating a stack
@@ -175,7 +191,7 @@ abline(0,1, col="red")
 plot(FCOVER1999, FCOVER2019, xlim=c(0, 1), ylim=c(0, 1), xlab="FCOVER 2014", ylab="FCOVER 2019")
 abline(0,1, col="red")
 
-# Plot frequency distribution of data
+# Plot frequency distribution of data: frequecies : how pixels are distributed in each different class - frequencies of each classes
 par(mfrow=c(3,2))
 hist(FCOVER1999)
 hist(FCOVER2004)
@@ -194,15 +210,103 @@ dif4 <- FCOVER2014 - FCOVER2019
 dif5 <- FCOVER1999 - FCOVER2019
 
 # Use ggplot with viridis palette to see differences between years
-d1 <- ggplot() + geom_raster(data = FCOVER1999 - FCOVER2019, mapping = aes(x=x, y=y, fill= dif5)) + scale_fill_viridis(option = "magma") + ggtitle("Percentage of forest loss and gain between 1999 and 2019")
-
-# Create a color palette with COLORBREWER 2.0 to better see differences
+d1 <- ggplot() + geom_raster(data = dif1, mapping = aes(x=x, y=y, fill= FCOVER1999 - FCOVER2004)) + scale_fill_viridis(option = "magma") + ggtitle("Percentage of forest loss and gain between 1999 and 2004")
+d2 <- ggplot() + geom_raster(data = FCOVER2004 - FCOVER2009, mapping = aes(x=x, y=y, fill= dif2)) + scale_fill_viridis(option = "magma") + ggtitle("Percentage of forest loss and gain between 2004 and 2009")
+d3 <- ggplot() + geom_raster(data = FCOVER2009 - FCOVER2014, mapping = aes(x=x, y=y, fill= dif3)) + scale_fill_viridis(option = "magma") + ggtitle("Percentage of forest loss and gain between 2009 and 2014")
+d4 <- ggplot() + geom_raster(data = FCOVER2014 - FCOVER2019, mapping = aes(x=x, y=y, fill= dif4)) + scale_fill_viridis(option = "magma") + ggtitle("Percentage of forest loss and gain between 2014 and 2019")
+d5 <- ggplot() + geom_raster(data = FCOVER1999 - FCOVER2019, mapping = aes(x=x, y=y, fill= dif5)) + scale_fill_viridis(option = "magma") + ggtitle("Percentage of forest loss and gain between 1999 and 2019")
+# Multiframe ggplot with patchwork
+d1 + d2 + d3 /  d4 + d5
+# Create a color palette with COLORBREWER 2.0 
 # x = 0 -> no changes: colored in white
 class3_RdBu <- colorRampPalette(colors = c('#ef8a62','#f7f7f7','#67a9cf'))(255)
-difw <- plot(dif5, col=class3_RdBu)
+difw <- plot(dif5, col=class3_RdBu, main = "Difference in Forest Cover between 1999 and 2019")
 # x = 0 -> no changes: colored in black
 class3_RdBu2 <- colorRampPalette(colors = c('#ef8a62','#636363','#7fbf7b'))(255)
-difb <- plot(dif5, col=class3_RdBu2)
+difb <- plot(dif5, col=class3_RdBu2, main = "Difference in Forest Cover between 1999 and 2019")
+
+# Plot together
+par(mfrow=c(1,2))
+difw 
+difb
+
+# Export
+pdf("fcoverdif.pdf", width=30, height=10) #migliorare
+par(mfrow=c(2,3))
+difw 
+difb
+dev.off()
+
+
+
+######
+
+
+setwd("/Users/sarapiccini/Documents/datandvii")
+rlist <- list.files(pattern="NDVI") # listing all the files with the pattern present in the directory
+rlist
+list_rast <- lapply(rlist, raster) # to make the list a brick list - apply brick function to all the files (multi-layers)
+list_rast
+NDVIstack <- stack(list_rast) # creating a stack
+NDVIstack
+plot(NDVIstack)
+
+ext <- c(94.5, 150, -11.5, 0)
+NDVIcrop <- crop(NDVIstack, ext)
+plot(NDVIcrop)
+NDVIcrop
+
+NDVI1999 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1km.1
+NDVI2004 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1km.2
+NDVI2009 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1km.3
+NDVI2014 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1km.4
+NDVI2019 <- NDVIcrop$Normalized.Difference.Vegetation.Index.1km.5
+
+# Plot them with ggplot 
+g1 <- ggplot() + geom_raster(data = NDVI1999, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km.1)) + scale_fill_viridis(option = "inferno") + ggtitle("NDVI in 1999") + labs(fill = "NDVI")
+g2 <- ggplot() + geom_raster(data = NDVI2004, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km.2)) + scale_fill_viridis(option = "inferno") + ggtitle("NDVI in 2004") + labs(fill = "NDVI")
+g3 <- ggplot() + geom_raster(data = NDVI2009, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km.3)) + scale_fill_viridis(option = "inferno") + ggtitle("NDVI in 2009") + labs(fill = "NDVI")
+g4 <- ggplot() + geom_raster(data = NDVI2014, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km.4)) + scale_fill_viridis(option = "inferno") + ggtitle("NDVI in 2014") + labs(fill = "NDVI")
+g5 <- ggplot() + geom_raster(data = NDVI2019, mapping = aes(x=x, y=y, fill=Normalized.Difference.Vegetation.Index.1km.5)) + scale_fill_viridis(option = "inferno") + ggtitle("NDVI in 2019") + labs(fill = "NDVI")
+
+# I have also tried this one: scale_fill_viridis(direction = -1, option = "magma") with the order of colors reversed but 
+# Plot them together (multiframe ggplot) with grid.arrange function (from gridExtra package)
+grid.arrange(g1, g2, g3, g4, g5, nrow=3)
+
+clNDVI <- colorRampPalette(colors = c('#6e462c', '#9c8448', 'white', '#9cab68', '#306466'))(255)
+
+par(mfrow=c(2,3))
+plot(NDVI1999, col = clNDVI)
+plot(NDVI2004, col = clNDVI)
+plot(NDVI2009, col = clNDVI)
+plot(NDVI2014, col = clNDVI)
+plot(NDVI2019, col = clNDVI)
+
+#ndvi difference between years
+NDVIdif <- NDVI1999 - NDVI2019
+#creation of a new color palette to emphasize differences with COLORBREWER 2.0:
+col <- colorRampPalette(c('#ef3b2c','#f7f7f7','#008837'))(255)
+plot(NDVIdif, col=col, main="Difference in NDVI between 1999 and 2019")
+
+######
+
+
+rfcover <- list.files(pattern="FCOVER")
+rfcover
+
+rfcover_rast <- lapply(rfcover, raster)
+rfcover_rast 
+
+# Creating a stack
+FCOVERstack <- stack(rfcover_rast) 
+FCOVERstack 
+plot(FCOVERstack)
+
+# Crop the image over Central Africa
+FCOVERcrop <- crop(FCOVERstack, ext)
+plot(FCOVERcrop)
+FCOVERcrop
+
 
 
 
